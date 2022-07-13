2794b7ece5be57651931ed6c324fb65e56229901
lokile <107489661+lokile@users.noreply.github.com>
2022-07-13 22:47:10 +0700

    improve library

--- appLibraries/src/main/java/com/lokile/applibraries/base/BaseApplication.kt
+++ appLibraries/src/main/java/com/lokile/applibraries/base/BaseApplication.kt
@@ -3,11 +3,9 @@ package com.lokile.applibraries.base
 import android.app.Application
 import android.content.Context
 import androidx.multidex.MultiDex
-import com.lokile.applibraries.managers.RemoteConfigValue
 import com.lokile.applibraries.managers.RxBus
 import com.lokile.applibraries.managers.RxBusListener
 import com.lokile.applibraries.utils.isLogException
-import com.lokile.firebase_analytics_support.initFirebase
 
 abstract class BaseApplication : Application(), RxBusListener {
     override val uuid: Int by lazy { RxBus.INSTANCE.newUUID() }
@@ -16,14 +14,6 @@ abstract class BaseApplication : Application(), RxBusListener {
         super.onCreate()
         app = this
         isLogException = hasCrashlytics()
-    }
-
-    fun setupFirebase(
-        remoteConfigList: List<RemoteConfigValue>,
-        logEventTracking: Boolean,
-        loadingCallback: ((loadFromPreviousVersion: Boolean, configUpdated: Boolean, fetchSuccess: Boolean) -> Unit)? = null
-    ) {
-        initFirebase(this, remoteConfigList, logEventTracking, loadingCallback)
     }
 
     inline fun <reified T> registerEventListener(crossinline callBack: (T) -> Unit) {
--- appLibraries/src/main/java/com/lokile/applibraries/utils/FirebaseUtils.kt
+++ appLibraries/src/main/java/com/lokile/applibraries/utils/FirebaseUtils.kt
@@ -1,9 +1,11 @@
 package com.lokile.applibraries.utils
 
+import com.google.firebase.crashlytics.FirebaseCrashlytics
+
 internal var isLogException = false
 fun handleException(throwable: Throwable) {
     throwable.printStackTrace()
-    if (isLogException){
-        com.lokile.firebase_analytics_support.handleException(throwable)
+    if (isLogException) {
+        FirebaseCrashlytics.getInstance().recordException(throwable)
     }
 }
