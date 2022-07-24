b43d239d760a3bc21919a4bf8f5fb0325b4433f8
lokile <107489661+lokile@users.noreply.github.com>
2022-07-24 12:24:53 +0700

    improve library

--- appLibraries/src/main/java/com/lokile/applibraries/services/BaseSharedPreferences.kt
+++ appLibraries/src/main/java/com/lokile/applibraries/services/BaseSharedPreferences.kt
@@ -2,54 +2,16 @@ package com.lokile.applibraries.services
 
 import android.content.Context
 import androidx.core.content.edit
-import com.lokile.encrypter.encrypters.imp.Encrypter
 
 
 open class BaseSharedPreferences(
-    val preferencesName: String,
+    preferencesName: String,
     context: Context,
-    val isEncrypt: Boolean = false
 ) {
-    private val keyMap = hashMapOf<String, String>()
-    private val enctypter by lazy {
-        Encrypter(
-            context = context,
-            alias = "alias" + preferencesName
-        )
-    }
     private val pref = context.getSharedPreferences(preferencesName, Context.MODE_PRIVATE)
-    fun convertKey(key: String): String {
-        return if (isEncrypt) {
-            keyMap.get(key)
-                ?: enctypter.encryptOrNull(key, false)?.also {
-                    keyMap[key] = it
-                } ?: key
-        } else {
-            key
-        }
-    }
-
-    fun encryptString(data: String): String {
-        return if (isEncrypt) {
-            enctypter.encryptOrNull(data) ?: data
-        } else {
-            data
-        }
-    }
-
-    fun decryptString(data: String?): String? {
-        return if (isEncrypt && !data.isNullOrEmpty()) {
-            enctypter.decryptOrNull(data) ?: data
-        } else {
-            data
-        }
-    }
-
-
-    protected fun setData(inputkey: String, data: Any) {
-        val key = convertKey(inputkey)
+    protected fun setData(key: String, data: Any) {
         when (data) {
-            is String -> pref.edit { putString(key, encryptString(data)) }
+            is String -> pref.edit { putString(key, data) }
             is Boolean -> pref.edit { putBoolean(key, data) }
             is Float -> pref.edit { putFloat(key, data) }
             is Int -> pref.edit { putInt(key, data) }
@@ -58,13 +20,9 @@ open class BaseSharedPreferences(
         }
     }
 
-    protected fun getString(key: String, default: String? = null) =
-        decryptString(pref.getString(convertKey(key), default))
-
-    protected fun getBoolean(key: String, default: Boolean = false) =
-        pref.getBoolean(convertKey(key), default)
-
-    protected fun getFloat(key: String, default: Float) = pref.getFloat(convertKey(key), default)
-    protected fun getInt(key: String, default: Int) = pref.getInt(convertKey(key), default)
-    protected fun getLong(key: String, default: Long) = pref.getLong(convertKey(key), default)
+    protected fun getString(key: String, default: String? = null) = pref.getString(key, default)
+    protected fun getBoolean(key: String, default: Boolean = false) = pref.getBoolean(key, default)
+    protected fun getFloat(key: String, default: Float) = pref.getFloat(key, default)
+    protected fun getInt(key: String, default: Int) = pref.getInt(key, default)
+    protected fun getLong(key: String, default: Long) = pref.getLong(key, default)
 }
